fb5bc33e14ef4de17265866ff1ec5c9b956ae507
Richard Hyesung O <orichh@gmail.com>
2021-11-17 14:04:00 -0500

    Setup express server with node-pg

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const pool = require('../db/index');
+
 const app = express();
 const port = 3000;
+
+pool.query('SELECT * FROM questions', (err, res) => {
+  console.log('hello');
+  console.log(err, res);
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
