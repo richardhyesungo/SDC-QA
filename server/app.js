44e7e0b68be411ab04fd5ea28cf7f82dcd5ac537
Richard Hyesung O <orichh@gmail.com>
2021-11-17 20:16:29 -0500

    Set up API endpoints

--- server/app.js
+++ server/app.js
@@ -7,6 +7,44 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// questions
+app.get('/qa/questions', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/qa/questions', (req, res) => {
+  res.send('Hello World!');
+});
+
+// answers
+app.get('/qa/questions/:question_id/answers', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/qa/questions/:question_id/answers', (req, res) => {
+  res.send('Hello World!');
+});
+
+// mark question helpful
+app.put('/qa/questions/:question_id/helpful', (req, res) => {
+  res.send('Hello World!');
+});
+
+// report a question
+app.put('/qa/questions/:question_id/report', (req, res) => {
+  res.send('Hello World!');
+});
+
+// mark question as helpful
+app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+  res.send('Hello World!');
+});
+
+// report answer
+app.put('/qa/answers/:answer_id/report', (req, res) => {
+  res.send('Hello World!');
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
