d233898b94ec95db51156ce856c8f5ad69745713
Richard Hyesung O <orichh@gmail.com>
2021-11-30 00:10:45 -0500

    Add more tests

--- server/tests/app.int.test.js
+++ server/tests/app.int.test.js
@@ -2,8 +2,14 @@ const request = require('supertest');
 const app = require('../app.js');
 
 // integration tests
-describe('test test', () => {
-  describe('test', () => {
+describe('Questions API endpoint tests', () => {
+  describe('add question', () => {
+    test('should test', async () => {
+      const response = await request(app).post('/qa/questions');
+      expect(response.statusCode).toBe(201);
+    });
+  });
+  describe('get question', () => {
     test('should test', async () => {
       const response = await request(app).get('/qa/questions');
       expect(response.statusCode).toBe(200);
